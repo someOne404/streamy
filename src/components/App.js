e1320e8ca7ea75e306fff7bd87a60ad63960fc80
someOne404 <jiayilu96@gmail.com>
2020-04-01 20:36:25 -0400

    Link Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
     return (
         <div>
             PageOne
-            BAD!!!
-            <a href="/pagetwo">Go to Page Two</a>
+            <Link to="/pagetwo">Go to Page Two</Link>
         </div>
     );
 };
@@ -16,8 +15,7 @@ const PageTwo = () => {
         <div>
             PageTwo
             <button>Click Me!</button>
-            BAD!!!
-            <a href="/">Go to Page One</a>
+            <Link to="/">Go to Page One</Link>
         </div>
     );
 };
