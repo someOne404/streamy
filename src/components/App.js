aa4fd119de6c7536201d175263cf8be8b05e1cb9
someOne404 <jiayilu96@gmail.com>
2020-04-03 18:21:51 -0400

    google oauth without redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './header';
 
-const PageOne = () => {
-    return (
-        <div>
-            PageOne
-            <Link to="/pagetwo">Go to Page Two</Link>
-        </div>
-    );
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo
-            <button>Click Me!</button>
-            <Link to="/">Go to Page One</Link>
-        </div>
-    );
-};
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                   <Header />
+                   <Route path="/" exact component={StreamList} />
+                   <Route path="/streams/new" exact component={StreamCreate} />
+                   <Route path="/streams/edit" exact component={StreamEdit} />
+                   <Route path="/streams/delete" exact component={StreamDelete} />
+                   <Route path="/streams/show" exact component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
