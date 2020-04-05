e09888f90246925aea19f7a2d31fb8a9eb0a8ace
someOne404 <jiayilu96@gmail.com>
2020-04-05 18:37:12 -0400

    new syntax 2: alternate way to render input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput(formProps) {
-        return <input {...formProps.input} />;
+    renderInput({ input }) {
+        return <input {...input} />;
     }
 
     render() {
